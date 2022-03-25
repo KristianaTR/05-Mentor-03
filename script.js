00f1afecaae977352a61de9791c0dde603b3b750
KristianaTR <kristiana.tatarcuka@gmail.com>
2022-03-25 11:32:31 +0200

    update 25032022

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var userInput = document.querySelector('.month_name').value; //iegūstu ievadīto vērtību - bet realitātē par to nevaru pārliecināties
+
+function enter(){
+    var userInput = document.querySelector('.month_name').value;
+   return userInput;
+}
+
 var months = ['janvāris', 'februāris', 'marts', 'aprīlis', 'maijs', 'jūnijs', 'jūlijs', 'augusts', 'septembris', 'oktobris', 'novembris', 'decembris'];
-var form = document.querySelector('.month_name');
 var indicator = document.querySelector('.circle_validation');
 
 function validation(){
-    for(i=0; i<= months.length; i++){
-        if(userInput == months[i]){
-            form.classList.add('valid');
-            form.classList.remove('invalid');
-            indicator.style.background = '#00ff00';
-        }
-        else {
-            form.classList.remove('valid');
-            form.classList.add('invalid');
-            indicator.style.background = '#ff0000';
-        }
+    var userInput = enter();
+    if (months.includes(userInput)){
+        indicator.style.background = '#00ff00';
     }
+    else{
+        indicator.style.background = '#ff0000';
     }
+
+}
