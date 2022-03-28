bc22068ddee260648bba6d51957777dc456fcfe4
KristianaTR <82210631+KristianaTR@users.noreply.github.com>
2022-03-28 16:10:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 
-function enter(){
-    var userInput = document.querySelector('.month_name').value;
-   return userInput;
-}
 
 var months = ['janvāris', 'februāris', 'marts', 'aprīlis', 'maijs', 'jūnijs', 'jūlijs', 'augusts', 'septembris', 'oktobris', 'novembris', 'decembris'];
 var indicator = document.querySelector('.circle_validation');
 
 function validation(){
-    var userInput = enter();
+    var Input = document.querySelector('.month_name').value;
+    var userInput = Input.toLowerCase();
     if (months.includes(userInput)){
         indicator.style.background = '#00ff00';
     }
